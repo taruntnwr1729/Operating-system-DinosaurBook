4b4b7dffbc652b01401c33df28bafd88432f67ce
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-07-06 17:55:37 +0530

    Improve comments and formatting in ReadWrite.cpp
    Add explanatory comments and small formatting/indentation cleanup in ReadWrite.cpp. Comments clarify POSIX includes, buffer size, read/write loop and error handling; moved/adjusted whitespace for readability. No functional changes were made.

--- ReadWrite.cpp
+++ ReadWrite.cpp
@@ -3,57 +3,60 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+//we first imported POSIX API TO COMMUNICATE WITH KERNEL
 using namespace std;
 
 int main(){
+    //SIMPLY DEFINED THE DATA TYPES
     string source_path;
     string dest_path;
-
+    //TOOK INPUT OF FILES TO WHICH CHANGES WILL BE MADE
     cout<<"enter the source file: ";
     cin>> source_path;
     cout<< "enter the destination file: ";
     cin>> dest_path;
 
-
+    //WE OPENED THE FILE
     int src_fd = open(source_path.c_str(), O_RDONLY);
-
+    //WE CHECKED IF THE OPEN FUNCTION RETURNED -1 TO US WE WILL DO ERROR HANDLING
     if (src_fd < 0){
         cerr<<"error: source file"<<source_path<<" does not exist. \n";
         return 1;
     }
+    //SIMILARLIY HERE
     int dest_fd = open(dest_path.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if(dest_fd < 0){
         cerr<<"error not able to create destination file. \n";
         close(src_fd);
         return 1;
     }
-
-    const int BUFFER_SIZE = 4096;
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read;
+    //THIS IS THE MAIN LOGIC TO COPY DATA
+    const int BUFFER_SIZE = 4096; //WE DEFINED HERE SIZE OF THE PAGE (4KB) WHICH WE WILL BE USING
+    char buffer[BUFFER_SIZE]; //HERE READ FUNCTION WILL DUMP DATA SO THAT WRITE FUN CAN GRAB IT 
+    ssize_t bytes_read; //DATA TYPE FOR NO. OF BYTES THAT ARE READ
 
     while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE))>0){
+        //IN THIS LOOP WE CHECK IF THERE IS ANY BYTE THAT HAS BEEN READ
         ssize_t bytes_written = write(dest_fd, buffer, bytes_read);
+        //BY THIS WE WRITE IT INTO THE DESTINATION FILE
 
         
 
         if(bytes_written != bytes_read){
+            //CONDITION TO CHECK IF EVERYTHING HAS BEEN WRITTEN OR NOT
 
-            cerr<<"Critical error, failed in writing data. \n";
+            cerr<<"Critical error, failed in writing data. \n";//IF NOT THEN GIVE ERROR
             close(src_fd);
             close(dest_fd);
 
             return 1;
 
         }
-
-
     }
 
-        if(bytes_read<0){
-            cerr<<"ERROR OCCURED WHILE READING. \n";
-        }
+    if(bytes_read<0){
+        cerr<<"ERROR OCCURED WHILE READING. \n";
+}
 
         close(src_fd);
         close(dest_fd);
